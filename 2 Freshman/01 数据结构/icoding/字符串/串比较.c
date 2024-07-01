6a81819696bcb6b650fba0fd2f3e06c0d3381181
fuxi <1538130391@qq.com>
2024-07-01 17:15:43 +0800

    2024-07-01
    更新icoding

--- 2 Freshman/01 数据结构/icoding/字符串/串比较.c
+++ 2 Freshman/01 数据结构/icoding/字符串/串比较.c
@@ -1,3 +1,16 @@
+/*
+串比较
+
+不调用库函数，自己实现字符串的比较操作：该操作当比较的两个字符是都是字母，
+ 且两个字符互为大小写（如a和A、e和E）时认为两个字符相同，否则不同，
+ 其比较结果按这两个字符的原值确定。函数的返回值规定如下：
+返回值 < 0：第一个不匹配的字符在 ptr1 中的值低于 ptr2 中的值
+返回值 == 0：两个字符串的内容相等
+返回值 > 0：第一个不匹配的字符在 ptr1 中的值大于在 ptr2 中的值
+
+函数原型如下：
+int str_compare(const char* ptr1, const char* ptr2);
+ */
 #include "dsstring.h" //请不要删除，否则检查不通过
 
 int str_compare(const char *ptr1, const char *ptr2) {
--- 2 Freshman/01 数据结构/icoding/字符串/块链串.c
+++ 2 Freshman/01 数据结构/icoding/字符串/块链串.c
@@ -1,5 +1,36 @@
 /*
+块链串
 
+块链串定义如下：
+
+#define BLOCK_SIZE 4    // 可由用户定义的块大小
+#define BLS_BLANK '#'   // 用于空白处的补齐字符
+
+typedef struct _block {
+    char ch[BLOCK_SIZE];    //块的数据域
+    struct _block *next;    //块的指针域
+} Block;
+
+typedef struct {
+    Block *head;        // 串的头指针
+    Block *tail;        // 串的尾指针
+    int len;            // 串的当前长度
+} BLString;
+
+//字符串初始化函数：
+void blstr_init(BLString *T) {
+    T->len = 0;
+    T->head = NULL;
+    T->tail = NULL;
+}
+这些定义已包含在头文件 dsstring.h 中，请实现块链串的子串查找操作：
+
+bool blstr_substr(BLString src, int pos, int len, BLString *sub);
+src为要查找的字符串
+pos为子串开始的下标
+len为子串的长度
+sub在函数调用运行前指向一个已经初始化好的空串，在函数返回时，sub指向串src从第pos个字符起长度为len的子串
+函数查找成功返回true，参数不正确返回 false
  */
 
 #include <stdlib.h>
